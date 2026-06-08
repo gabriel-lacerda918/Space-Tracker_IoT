57a0b22aac1748a5272d8f44472c8c5ebe784d3a
Gabriel Lacerda <161903643+gabriel-lacerda918@users.noreply.github.com>
2026-06-08 12:48:03 -0300

    Delete src directory

--- src/calibration.py
+++
@@ -1,9 +0,0 @@
-KNOWN_WIDTH_CM = 8.56
-
-distance_reference_cm = float(input("Distancia conhecida (cm): "))
-width_pixels = float(input("Largura observada (pixels): "))
-
-focal = (width_pixels * distance_reference_cm) / KNOWN_WIDTH_CM
-
-print(f"\nFOCAL_LENGTH calculado = {focal:.2f}")
-print("Copie esse valor para o main.py")
